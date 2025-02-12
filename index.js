d1365b077a2c5e860ef397f8c6d464a382eb77dc
Basil Khwaja <khwajabasil@gmail.com>
2025-02-12 11:34:35 -0500

    fix background image

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.button_content .button:first-child').addEventListener('click', function () {
     document.getElementById('blastoiseImage').src = './gifs/nanami-touch.gif'; // Change to your new image path
     document.getElementById('yippee').src = './gifs/yippee-cat.gif';
-    document.getElementById('pika').src = '//gifs/pika-giphy.gif';
+    document.getElementById('pika').src = './gifs/pika-giphy.gif';
     document.getElementById('yippee').style.display = 'block';
     document.getElementById('pika').style.display = 'block';
     document.querySelector('.title').textContent = "GET READY TO BE WINE AND DINED. UR MINNNEEE NOW!!!!"; // Change title text for Yes
